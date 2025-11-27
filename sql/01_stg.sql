a472c1e6b6c4897f0b4ffc6f4b6b2bd8b2337bb7
Daria Pantiushina <the1007pan.da@gmail.com>
2025-11-27 18:04:09 +0100

    Add files via upload

--- sql/01_stg.sql
+++ sql/01_stg.sql
@@ -1,1 +1,23 @@
+-- ============================
+-- STAGING: Customers
+-- ============================
+CREATE TABLE stg_customers AS
+SELECT
+    customer_id,
+    start_date,
+    "group",
+    user_type
+FROM raw_customers
+WHERE start_date IS NOT NULL;
 
+-- ============================
+-- STAGING: Activities
+-- ============================
+CREATE TABLE stg_activities AS
+SELECT
+    customer_id,
+    DATE(event_ts) AS event_date,
+    event_ts,
+    event_type
+FROM raw_activity_logs
+WHERE customer_id IS NOT NULL;
